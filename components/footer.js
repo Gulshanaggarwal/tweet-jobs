c526991fb208d228ae055d6c1a673998d1d4d6a2
Ishan <56102033+ishanjain1502@users.noreply.github.com>
2022-02-18 21:45:38 +0530

    footer2

--- components/footer.js
+++ components/footer.js
@@ -1,14 +1,20 @@
-import Image from 'next/image'
-import { default as Link } from 'next/link'
-import React from 'react'
+import Image from 'next/image';
+import { default as Link } from 'next/link';
+import React from 'react';
 const footer = () => {
+    const src = 'https://img.icons8.com/ios-glyphs/30/000000/github.png';
     return (
         <div className='bg-twitter p-4' >
             <Link href="/">
                 <a> <p className=' font-bold text-2xl text-center text-stone-50' >TweetJOBS</p> </a>
             </Link>
             <br />
-            <Image src='/github.png' width={30} height={30} />
+            <div className='flex flex-col justify-center items-center' >
+                <Link href='/' ><a>
+                    <Image className='text-center' loader={() => src} src='https://img.icons8.com/ios-glyphs/30/000000/github.png' height={70} width={70} />
+                </a>
+                </Link>
+            </div>
             <br />
             <div className='p-0 text-center text-stone-50' >
                 Made with 🧡 by <a href='https://twitter.com/gulshanagg333'>Gulshan</a> and <a href='https://twitter.com/ishanjain_'>Ishan</a>
